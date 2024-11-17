54ab1271412c2a30355cd97875ee243e403836c7
Sarfaraz Qadir <sarfarazqadir594@gmail.com>
2024-11-17 14:38:28 +0500

    first

---
+++ script.js
@@ -0,0 +1,62 @@
+// 1
+
+// var num = new Date();
+// document.write(num)
+
+// 2
+
+// var num = new Date();
+// var a =num.getMonth()
+// var b = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+// document.write(b[a]);
+
+// 3
+
+// var num = new Date();
+// var a =num.getDay()
+// console.log(a);
+
+// var b = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+// document.write(b[a].slice(0,3));
+
+// 4
+
+// var num = new Date();
+// var a = num.getDay()
+// console.log(a);
+
+// var b = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+// var c = (b[a]);
+
+// if(c == "Saturday" || c == "Sunday"){
+//     document.write("Today is a Funday");
+// }
+
+// 5
+
+// var num = new Date();
+// var a = num.getDay()
+ 
+// if(a <= 15){
+//     document.write("First Fifteen of the month");
+// }else{
+//     document.write("Last Fifteen of the month");
+// }
+
+// 6
+
+// var num = new Date();
+// document.write("Current Date : "+num);
+// var a = num.getTime() // for Seconds
+// document.write("<br>Elapsed miliseconds since January 1, 1970 : "+a);
+// var a = num.getTime()/1000/60 // for Minutes
+// document.write("<br>Elapsed Minutes since January 1, 1970 : "+a);
+
+// 7
+
+
+// var date = new Date();
+
+
+
+
